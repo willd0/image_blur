de03e7c615393bdafb3ed3434ac379ba4ad43fde
Will Daskam <willd2122@gmail.com>
2019-07-24 19:50:28 +0000

    image_blur #2

--- image_blur.rb
+++ image_blur.rb
@@ -1,13 +1,46 @@
-class Image
+  class Image
   
-  def initialize(a)
-    @image = a
-  end
-  
-  def output_image
-    @image.each do |x|
-        puts "#{x.join}"
+    def initialize(array)
+      @image = array
     end
+  
+    def output_image
+      @image.each do |output|
+          puts "#{output.join}"
+      end
+    end
+    
+  def blur
+  values_to_change = []
+  @image.each_with_index do |row, row_index|
+    row.each_with_index do |value, value_index|
+      if value == 1
+      # above
+        if row_index != 0
+        values_to_change << [row_index - 1, value_index]
+        end
+      # below
+        if row_index != @image.length - 1
+        values_to_change << [row_index + 1, value_index]
+        end
+      # right
+        if value_index != row.length - 1
+        values_to_change << [row_index, value_index + 1]
+        end
+      # left
+        if value_index != 0
+        values_to_change << [row_index, value_index - 1]
+        end
+      end 
+    end
+  end
+  # after looping through all rows and columns
+  values_to_change.each do |stored_values|
+    # [row_index, column_index] of coordinate to change to 1
+    # row: stored_values[0]
+    # column: stored_values[1]
+    @image[stored_values[0]][stored_values[1]] = 1
+  end
   end
 end
   
@@ -17,4 +50,7 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
+image.output_image
+puts ""
+image.blur
 image.output_image
