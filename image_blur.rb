0bd8d73bc13424aa3692c0dbb1ceb2f7ad09710e
Will Daskam <willd2122@gmail.com>
2019-08-06 19:26:52 +0000

    Refactored code

--- image_blur.rb
+++ image_blur.rb
@@ -11,42 +11,41 @@ class Image
     end
     
     def blur(distance)
-    check_distance = distance - 1
-      while check_distance >= 0
+      while distance > 0
         values_to_change = []
-  @image.each_with_index do |row, row_index|
-    row.each_with_index do |value, value_index|
-      if value == 1
-      # above
-        if row_index != 0
-        values_to_change << [row_index - 1, value_index]
-        end
+        @image.each_with_index do |row, row_index|
+          row.each_with_index do |value, value_index|
+            if value == 1
+            # above
+              if row_index != 0
+                values_to_change << [row_index - 1, value_index]
+              end
         
-       # alternate version: values_to_change << [row_index -1, value_index] if row_index !=0
+              # alternate version: values_to_change << [row_index -1, value_index] if row_index !=0
         
-      # below
-        if row_index != @image.length - 1
-        values_to_change << [row_index + 1, value_index]
-        end
-      # right
-        if value_index != row.length - 1
-        values_to_change << [row_index, value_index + 1]
-        end
-      # left
-        if value_index != 0
-        values_to_change << [row_index, value_index - 1]
-        end
-      end 
-    end
-  end
-  # after looping through all rows and columns
-  values_to_change.each do |stored_values|
-    # [row_index, column_index] of coordinate to change to 1
-    # row: stored_values[0]
-    # column: stored_values[1]
-    @image[stored_values[0]][stored_values[1]] = 1
-  end
-      check_distance = check_distance - 1
+              # below
+              if row_index != @image.length - 1
+                values_to_change << [row_index + 1, value_index]
+              end
+              # right
+              if value_index != row.length - 1
+                values_to_change << [row_index, value_index + 1]
+              end
+              # left
+              if value_index != 0
+                values_to_change << [row_index, value_index - 1]
+              end
+            end 
+         end
+      end
+      # after looping through all rows and columns
+      values_to_change.each do |stored_values|
+      # [row_index, column_index] of coordinate to change to 1
+      # row: stored_values[0]
+      # column: stored_values[1]
+        @image[stored_values[0]][stored_values[1]] = 1
+      end
+      distance = distance - 1
       end
     end
   end
